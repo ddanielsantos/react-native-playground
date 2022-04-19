d93584ccc9cd4f227560721638b270d3d6c23cd5
Daniel Santos <contatodanieljob@gmail.com>
2022-04-19 00:50:09 -0300

    refactor: :zap:

--- supabase-realtime/src/components/TextInput/TextInput.tsx
+++ supabase-realtime/src/components/TextInput/TextInput.tsx
@@ -2,27 +2,27 @@ import React from 'react'
 import { TextInput as TI, TextInputProps } from 'react-native'
 
 type Props = TextInputProps & {
-  placeholder: string,
-  value: string,
   updater: React.Dispatch<React.SetStateAction<string>>,
-  hideCharacters?: boolean,
+  hideCharacters?: boolean
 }
 
-export const TextInput = ({ placeholder, updater, value, hideCharacters, style, ...props }: Props) => {
+export const TextInput = ({ updater, hideCharacters, style, ...props }: Props) => {
+
   return (
     <TI
-      placeholder={placeholder}
       placeholderTextColor={"#ffffff7e"}
-      value={value}
       secureTextEntry={false || hideCharacters}
-      style={[{
-        backgroundColor: '#1a0057',
-        borderRadius: 4,
-        padding: 10,
-        margin: 2,
-        marginHorizontal: 4,
-        color: 'white'
-      }, style]}
+      style={
+        [{
+          backgroundColor: '#1a0057',
+          borderRadius: 4,
+          padding: 10,
+          margin: 2,
+          marginHorizontal: 4,
+          color: 'white'
+        },
+          style]
+      }
       onChangeText={t => {
         updater(t)
       }}
