2464f18a8273f4b60cdc549927f9430023b004fe
Daniel Santos <contatodanieljob@gmail.com>
2022-04-19 00:26:41 -0300

    feat: :sparkles: add toast

--- supabase-realtime/App.tsx
+++ supabase-realtime/App.tsx
@@ -1,16 +1,17 @@
 import 'react-native-url-polyfill/auto'
 import { StatusBar } from 'expo-status-bar';
 import { NavigationContainer } from '@react-navigation/native'
+import { ToastProvider } from 'react-native-toast-notifications'
 
 import { AuthRoute } from './src/routes/AuthRoute';
 
 export default function App() {
   return (
-    <>
+    <ToastProvider>
       <NavigationContainer>
         <StatusBar style="light" translucent={false} backgroundColor={'#0f0131'} />
         <AuthRoute />
       </NavigationContainer>
-    </>
+    </ToastProvider>
   );
 }
